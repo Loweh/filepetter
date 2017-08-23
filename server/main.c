a0403a5fb92bbde7c1f8ce26af946def0f9ba640
Loweh <colder.than.zero@gmail.com>
2017-08-22 21:36:26 -0400

    server protocol implementation
    it's a start, but nobody ever said starts were glamourous. reverses the process of reducing the client to 4 chars, and keeps the entire file as a char, then outputted

--- server/main.c
+++ server/main.c
@@ -7,6 +7,8 @@ int main() {
 	WSADATA wsa;
 	SOCKET sock, clientsock;
 	struct sockaddr_in server, client;
+	int packetsize = 10;
+	int protocolsize = 6;
 	int returnvalue;
 
 	returnvalue = WSAStartup(MAKEWORD(2, 2), &wsa);
@@ -32,19 +34,37 @@ int main() {
 
 	int clientsize = sizeof(server);
 	clientsock = accept(sock, (struct sockaddr *)&client, &clientsize);
-	bool isAlive = true;
+
+	char *file; //temporary
+	int chunks = 0; //also temporary
+	int isAlive = 1;
 	while (isAlive) {
-		char buffer[2];
-		returnvalue = recv(clientsock, buffer, sizeof(buffer), 0);
+		char *buffer = (char *)malloc(packetsize + protocolsize);
+		returnvalue = recv(clientsock, buffer, packetsize + protocolsize, 0);
 		if (returnvalue > 0) {
-			buffer[1] = '\0';
-			printf("Message received: %s\n", buffer);
-		}
-		else {
+			if (buffer[0] == '1') {
+				int filesize = 0;
+				for (int i = 0; i < 4; i++) {
+					int filesizebyte = buffer[i + 1];
+					filesizebyte = filesizebyte << 8 * (3 - i);
+					filesize = filesize | filesizebyte;
+				};
+
+				file = (char *)malloc(filesize);
+			};
+
+			strncpy(file + (10 * chunks), buffer + protocolsize - 1, packetsize);
+			chunks++;
+		} else {
 			isAlive = false;
+			free(buffer);
 		};
 	};
 
+	printf("%s", file);
+	while (1) {
+		//more debugging stuff
+	};
 	closesocket(sock);
 	WSACleanup();
 	return 0;
