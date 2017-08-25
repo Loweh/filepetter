e8844d40c18b43ec3705392077bafb16598612d7
Loweh <colder.than.zero@gmail.com>
2017-08-25 09:39:30 -0400

    cleaning up a bit
    making sure this code isn't completely unreadable
    i might add more stuff if i find a way to transfer binary files, but they corrupt for some reason

--- client/main.c
+++ client/main.c
@@ -4,57 +4,63 @@
 #include <string.h>
 #pragma comment(lib, "ws2_32.lib")
 
+struct socketData {
+	WSADATA wsa;
+	SOCKET sock;
+	struct sockaddr_in server;
+	char *serverip = "127.0.0.1";
+	int port = 2127;
+};
+
 struct fileTransferData {
-	int fileid;
+	FILE *file;
+	int packetid;
 	int totalsize;
 	int chunks;
 	int chunksize = 10; //size of each packet
-	int protocolsize = 6; //1 character for fileid, 4 for length  of rawfile, 1 for null terminator
+	int protocolsize = 5; //1 character for packetid, 4 for length  of rawfile
 	char *filename;
 	char *rawfile;
 	char **segments;
 };
 
 int main() {
-	WSADATA wsa;
-	SOCKET sock;
-	struct sockaddr_in server;
-	FILE *testfile;
+	socketData sockdata;
 	fileTransferData ftdata;
 	int returnvalue;
 
-	returnvalue = WSAStartup(MAKEWORD(2, 2), &wsa);
+	returnvalue = WSAStartup(MAKEWORD(2, 2), &sockdata.wsa);
 	if (returnvalue != 0) {
 		return 1;
 	};
 
-	sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-	if (sock == INVALID_SOCKET) {
+	sockdata.sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if (sockdata.sock == INVALID_SOCKET) {
 		return 2;
 	};
 
-	server.sin_addr.s_addr = inet_addr("127.0.0.1");
-	server.sin_family = AF_INET;
-	server.sin_port = htons(2127);
+	sockdata.server.sin_addr.s_addr = inet_addr(sockdata.serverip);
+	sockdata.server.sin_family = AF_INET;
+	sockdata.server.sin_port = htons(sockdata.port);
 
-	returnvalue = connect(sock, (struct sockaddr *)&server, sizeof(server));
+	returnvalue = connect(sockdata.sock, (struct sockaddr *)&sockdata.server, sizeof(sockdata.server));
 	if (returnvalue == SOCKET_ERROR) {
 		return 3;
 	};
 
 	ftdata.filename = "test.txt";
-	testfile = fopen(ftdata.filename, "rb");
-	if (testfile == NULL) {
+	ftdata.file = fopen(ftdata.filename, "rb");
+	if (ftdata.file == NULL) {
 		return 4;
 	};
 
-	fseek(testfile, 0, SEEK_END);
-	ftdata.totalsize = ftell(testfile);
-	rewind(testfile);
+	fseek(ftdata.file, 0, SEEK_END);
+	ftdata.totalsize = ftell(ftdata.file);
+	rewind(ftdata.file);
 
-	ftdata.rawfile = (char *)malloc(ftdata.totalsize + 1);
-	fread(ftdata.rawfile, ftdata.totalsize, 1, testfile);
-	ftdata.rawfile[ftdata.totalsize] = '\0';
+	ftdata.rawfile = (char *)malloc(ftdata.totalsize);
+	fread(ftdata.rawfile, ftdata.totalsize, 1, ftdata.file);
+	fclose(ftdata.file);
 
 	returnvalue = ftdata.totalsize % ftdata.chunksize;
 	if (returnvalue == 0) {
@@ -64,14 +70,14 @@ int main() {
 		ftdata.chunks = (ftdata.totalsize / ftdata.chunksize) + 2; //would be + 1 to round up, but again, must account for the first packet
 	};
 
-	ftdata.fileid = 1;
+	ftdata.packetid = 1;
 	ftdata.segments = (char **)malloc(sizeof(char *) * ftdata.chunks);
 	for (int i = 0; i < ftdata.chunks; i++) {
 		int marker = (i - 1) * ftdata.chunksize;
 		char * chunk = (char *)malloc(ftdata.chunksize + ftdata.protocolsize);
 
 		if (i == 0) {
-			chunk[0] = ftdata.fileid + '0'; //for some reason this works
+			chunk[0] = ftdata.packetid + '0'; //for some reason this works
 
 			//convert ftdata.totalsize to a series of 4 chars
 			for (int i = 0; i < ftdata.protocolsize - 1; i++) {
@@ -81,28 +87,27 @@ int main() {
 				chunk[i + 1] = (char)totalsizebyte;
 			};
 
-			strncpy((chunk + ftdata.protocolsize) - 1, ftdata.filename, ftdata.chunksize);
+			strncpy(chunk + ftdata.protocolsize, ftdata.filename, ftdata.chunksize);
 		} else { //keep packets the same size without resending packet data
 			for (int y = 0; y < 5; y++) {
 				chunk[y] = '.';
 			};
 
-			strncpy((chunk + ftdata.protocolsize) - 1, ftdata.rawfile + marker, ftdata.chunksize);
+			strncpy(chunk + ftdata.protocolsize, ftdata.rawfile + marker, ftdata.chunksize);
 		};
 
-		chunk[(ftdata.chunksize + ftdata.protocolsize) - 1] = '\0'; //null terminate
 		ftdata.segments[i] = chunk;
 	};
 
 	for (int i = 0; i < ftdata.chunks; i++) {
-		send(sock, ftdata.segments[i], ftdata.chunksize + ftdata.protocolsize, 0);
+		send(sockdata.sock, ftdata.segments[i], ftdata.chunksize + ftdata.protocolsize, 0);
+		free(ftdata.segments[i]);
 	};
 
-	while (1) {
-		//not even sure what this is for anymore
-	};
+	free(ftdata.rawfile);
+	free(ftdata.segments);
 
-	closesocket(sock);
+	closesocket(sockdata.sock);
 	WSACleanup();
 	return 0;
 };
--- server/main.c
+++ server/main.c
@@ -3,9 +3,16 @@
 #include <ws2tcpip.h>
 #pragma comment(lib, "ws2_32.lib")
 
+struct socketData {
+	int port = 2127;
+	WSADATA wsa;
+	SOCKET sock, clientsock;
+	struct sockaddr_in server, client;
+};
+
 struct fileTransferData {
 	int chunksize = 10;
-	int protocolsize = 6;
+	int protocolsize = 5;
 	int filesize = 0;
 	int bytesreceived = 0;
 	char* filename;
@@ -13,40 +20,38 @@ struct fileTransferData {
 };
 
 int main() {
-	WSADATA wsa;
-	SOCKET sock, clientsock;
-	struct sockaddr_in server, client;
+	socketData sockdata;
 	fileTransferData ftdata;
 	int returnvalue;
 
-	returnvalue = WSAStartup(MAKEWORD(2, 2), &wsa);
+	returnvalue = WSAStartup(MAKEWORD(2, 2), &sockdata.wsa);
 	if (returnvalue != 0) {
 		return 1;
 	};
 
-	sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-	if (sock == INVALID_SOCKET) {
+	sockdata.sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if (sockdata.sock == INVALID_SOCKET) {
 		return 2;
 	};
 
-	server.sin_addr.s_addr = INADDR_ANY;
-	server.sin_family = AF_INET;
-	server.sin_port = htons(2127);
+	sockdata.server.sin_addr.s_addr = INADDR_ANY;
+	sockdata.server.sin_family = AF_INET;
+	sockdata.server.sin_port = htons(sockdata.port);
 
-	returnvalue = bind(sock, (struct sockaddr *)&server, sizeof(server));
+	returnvalue = bind(sockdata.sock, (struct sockaddr *)&sockdata.server, sizeof(sockdata.server));
 	if (returnvalue == SOCKET_ERROR) {
 		return 3;
 	};
 
-	listen(sock, 3);
+	listen(sockdata.sock, 3);
 
-	int clientsize = sizeof(server);
-	clientsock = accept(sock, (struct sockaddr *)&client, &clientsize);
+	int clientsize = sizeof(sockdata.server);
+	sockdata.clientsock = accept(sockdata.sock, (struct sockaddr *)&sockdata.client, &clientsize);
 
 	int isAlive = 1;
 	while (isAlive) {
 		char *buffer = (char *)malloc(ftdata.chunksize + ftdata.protocolsize);
-		returnvalue = recv(clientsock, buffer, ftdata.chunksize + ftdata.protocolsize, 0);
+		returnvalue = recv(sockdata.clientsock, buffer, ftdata.chunksize + ftdata.protocolsize, 0);
 		if (returnvalue > 0) {
 			if (buffer[0] == '1') {
 				for (int i = 0; i < 4; i++) {
@@ -55,16 +60,14 @@ int main() {
 					ftdata.filesize = ftdata.filesize | filesizebyte;
 				};
 
-				printf("%i", ftdata.filesize);
-				ftdata.filename = buffer + ftdata.protocolsize - 1;
+				ftdata.filename = buffer + ftdata.protocolsize;
 				ftdata.rawfile = (char *)malloc(ftdata.filesize);
 			}
 			else {
-				strncpy(ftdata.rawfile + (ftdata.bytesreceived), buffer + ftdata.protocolsize - 1, ftdata.chunksize);
+				strncpy(ftdata.rawfile + ftdata.bytesreceived, buffer + ftdata.protocolsize, ftdata.chunksize);
 				ftdata.bytesreceived = ftdata.bytesreceived + ftdata.chunksize;
 			};
 
-			//pretty much the only place i can probably fit this (please kill me i hate my code)
 			if (ftdata.bytesreceived >= ftdata.filesize) {
 				FILE *file = fopen(ftdata.filename, "wb");
 				fwrite(ftdata.rawfile, ftdata.filesize, 1, file);
@@ -76,7 +79,7 @@ int main() {
 		};
 	};
 
-	closesocket(sock);
+	closesocket(sockdata.sock);
 	WSACleanup();
 	return 0;
 };
